a5bffb9e21c2073fa0053daa2638818b2c91ab06
if12b075 <if12b075@technikum-wien.at>
2014-01-11 15:47:31 +0100

    DatabasehandlerImpl started

---
+++ src/at/technikum/wien/bif12/dbs/verwaltung/dao/mock/DatabaseHandlerImpl.java
@@ -0,0 +1,109 @@
+package at.technikum.wien.bif12.dbs.verwaltung.dao.mock;
+
+import java.util.List;
+
+import at.technikum.wien.bif12.dbs.verwaltung.dao.DatabaseHandler;
+import at.technikum.wien.bif12.dbs.verwaltung.entities.Anwesenheitsliste;
+import at.technikum.wien.bif12.dbs.verwaltung.entities.Course;
+import at.technikum.wien.bif12.dbs.verwaltung.entities.Lektor;
+import at.technikum.wien.bif12.dbs.verwaltung.entities.Lesson;
+import at.technikum.wien.bif12.dbs.verwaltung.entities.Semester;
+import at.technikum.wien.bif12.dbs.verwaltung.entities.Student;
+import at.technikum.wien.bif12.dbs.verwaltung.entities.Studiengang;
+import at.technikum.wien.bif12.dbs.verwaltung.entities.Template;
+import at.technikum.wien.bif12.dbs.verwaltung.entities.Zeugnis;
+
+public class DatabaseHandlerImpl implements DatabaseHandler {
+
+	@Override
+	public boolean addLektor(Lektor l) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addStudent(Student s) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addStudiengang(Studiengang s) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addTemplate(Template t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addSemester(Semester s) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean assignCourseToSemester(Course c, long semesterId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addLesson(Lesson l) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean registerGrade(long studentId, long courseId, int grade) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Zeugnis ladeZeugnis(long studenId, String semesterToken) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Lesson> ladeStundenplan(long studentId, String dayStart,
+			String dayEnd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Anwesenheitsliste ladeAnwesenheitsliste(long courseId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Course ladeFreifacher() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean assignStudentToCourse(long studentId, long courseId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<String> ladeStudiengaenge() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> ladeGehaltsklassen() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
