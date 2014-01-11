e83dc8d5903fdda306e7662ce23a156730dfaa9c
if12b075 <if12b075@technikum-wien.at>
2014-01-11 16:26:43 +0100

    DatabasHandlerImpl Grundaufbau

---
+++ src/at/technikum/wien/bif12/dbs/verwaltung/test/Main.java
@@ -0,0 +1,19 @@
+package at.technikum.wien.bif12.dbs.verwaltung.test;
+
+import at.technikum.wien.bif12.dbs.verwaltung.dao.mock.DatabaseHandlerMock;
+import at.technikum.wien.bif12.dbs.verwaltung.entities.Lektor;
+import at.technikum.wien.bif12.dbs.verwaltung.entities.Student;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DatabaseHandlerMock dhm = new DatabaseHandlerMock();
+		//dhm.addLektor(new Lektor("penis","klaus","abs","1200","123412341","penx@asdfa.com","penx",1));
+		//dhm.addStudent(new Student("penis","klaus","abs","1200","123412341","penx@asdfa.com","Informatik",1210234089,"heinz"));
+		
+		
+		
+
+	}
+
+}
