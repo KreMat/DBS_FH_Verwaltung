1320ab6f501b8389e0f16b241879cf741d0ab2a6
if12b075 <if12b075@technikum-wien.at>
2014-01-11 19:45:26 +0100

    some changes

--- src/at/technikum/wien/bif12/dbs/verwaltung/dao/mock/DatabaseHandlerImpl.java
+++ src/at/technikum/wien/bif12/dbs/verwaltung/dao/mock/DatabaseHandlerImpl.java
@@ -3,6 +3,8 @@ package at.technikum.wien.bif12.dbs.verwaltung.dao.mock;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -54,13 +56,13 @@ public class DatabaseHandlerImpl implements DatabaseHandler {
 			cs.setString(6, l.getEmail());
 			cs.setString(7, l.getToken());
 			cs.setString(8, l.getGehaltsklasse());
-			
+
 			cs.registerOutParameter(9, java.sql.Types.INTEGER);
-			
+
 			cs.executeUpdate();
 			int erg = cs.getInt(9);
-			
-			if(erg==0)
+
+			if (erg == 0)
 				return true;
 			else
 				return false;
@@ -87,19 +89,20 @@ public class DatabaseHandlerImpl implements DatabaseHandler {
 			cs.setString(7, s.getCourse_of_studies_name());
 			cs.setLong(8, s.getStudentnr());
 			cs.setString(9, s.getToken());
-			
+
 			cs.registerOutParameter(10, java.sql.Types.INTEGER);
-			
+
 			cs.executeUpdate();
-			
+
 			int erg = cs.getInt(10);
-			
-			if(erg==0)
+
+			if (erg == 0)
 				return true;
-			else if(erg==-1)
+			else if (erg == -1)
 				return false;
 			else {
-				System.out.println("Sorry there are to many students for that course!");
+				System.out
+						.println("Sorry there are to many students for that course!");
 				return false;
 			}
 		} catch (SQLException e) {
@@ -124,26 +127,114 @@ public class DatabaseHandlerImpl implements DatabaseHandler {
 
 	@Override
 	public boolean addSemester(Semester s) {
-		// TODO Auto-generated method stub
-		return false;
+		String CREATE_SEMESTER = "{call usp_create_semester(?,?,?,?)}";
+		try {
+			CallableStatement cs = con.prepareCall(CREATE_SEMESTER);
+
+			cs.setString(1, s.getToken());
+			cs.setString(2, s.getStart_day());
+			cs.setString(3, s.getEnd_day());
+
+			cs.registerOutParameter(4, java.sql.Types.INTEGER);
+
+			cs.executeUpdate();
+
+			int erg = cs.getInt(4);
+
+			if (erg == 0)
+				return true;
+			else
+				return false;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Failed to create semester!");
+			return false;
+		}
 	}
 
 	@Override
 	public boolean assignCourseToSemester(Course c, long semesterId) {
-		// TODO Auto-generated method stub
-		return false;
+		String ASSIGN_COURSE_TO_SEMESTER = "{call usp_assign_semester(?,?,?)}";
+		try {
+			CallableStatement cs = con.prepareCall(ASSIGN_COURSE_TO_SEMESTER);
+
+			cs.setLong(1, c.getCourseOfStudiesId());
+			cs.setLong(2, semesterId);
+
+			cs.registerOutParameter(3, java.sql.Types.INTEGER);
+
+			cs.executeUpdate();
+
+			int erg = cs.getInt(3);
+
+			if (erg == 0)
+				return true;
+			else
+				return false;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Failed to assign course to semester!");
+			return false;
+		}
 	}
 
 	@Override
 	public boolean addLesson(Lesson l) {
-		// TODO Auto-generated method stub
-		return false;
+		String CREATE_LESSON = "{call usp_create_lesson(?,?,?,?,?)}";
+		try {
+			CallableStatement cs = con.prepareCall(CREATE_LESSON);
+
+			cs.setLong(1, l.getCourse_id());
+			cs.setLong(2, l.getRoom_id());
+			cs.setString(3, l.getStart_time());
+			cs.setString(4, l.getEnd_time());
+
+			cs.registerOutParameter(5, java.sql.Types.INTEGER);
+
+			cs.executeUpdate();
+
+			int erg = cs.getInt(5);
+
+			if (erg == 0)
+				return true;
+			else
+				return false;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Failed to create lesson!");
+			return false;
+		}
 	}
 
 	@Override
 	public boolean registerGrade(long studentId, long courseId, int grade) {
-		// TODO Auto-generated method stub
-		return false;
+		String REGISTER_GRADE = "{call usp_register_grade(?,?,?,?)}";
+		try {
+			CallableStatement cs = con.prepareCall(REGISTER_GRADE);
+
+			cs.setLong(1, studentId);
+			cs.setLong(2, courseId);
+			cs.setInt(3, grade);
+
+			cs.registerOutParameter(4, java.sql.Types.INTEGER);
+
+			cs.executeUpdate();
+
+			int erg = cs.getInt(4);
+
+			if (erg == 0)
+				return true;
+			else
+				return false;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Failed to register grade!");
+			return false;
+		}
 	}
 
 	@Override
@@ -161,8 +252,36 @@ public class DatabaseHandlerImpl implements DatabaseHandler {
 
 	@Override
 	public Anwesenheitsliste ladeAnwesenheitsliste(long courseId) {
-		// TODO Auto-generated method stub
-		return null;
+		String LOAD_REGISTERLIST = "SELECT * FROM uv_create_register_list WHERE course_id = ?;";
+		String GET_COURSE_NAME = "SELECT id, name FROM tb_course_template WHERE id = ?;";
+		Anwesenheitsliste awhl = new Anwesenheitsliste();
+		try {
+			PreparedStatement getName = con.prepareStatement(GET_COURSE_NAME);
+			getName.setLong(1, courseId);
+			ResultSet name = getName.executeQuery();
+			awhl.setLehrveranstaltung(name.getString("name"));
+
+			PreparedStatement ps = con.prepareStatement(LOAD_REGISTERLIST);
+			ps.setLong(1, courseId);
+
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				long studentNr = rs.getLong("studentennr");
+				String firstname = rs.getString("firstname");
+				String lastname = rs.getString("lastname");
+
+				String entry = studentNr + " " + firstname + " " + lastname;
+				awhl.getNames().add(entry);
+			}
+			return awhl;
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Couldnt make a registerlist");
+			return null;
+		}
+
 	}
 
 	@Override
--- src/at/technikum/wien/bif12/dbs/verwaltung/entities/Course.java
+++ src/at/technikum/wien/bif12/dbs/verwaltung/entities/Course.java
@@ -1,7 +1,32 @@
 package at.technikum.wien.bif12.dbs.verwaltung.entities;
 
 public class Course {
+	private long courseOfStudiesId;
+	private long courseTemplateId;
+	private long semesterId;
 
-	// TODO DEFINE COURSE = Lehrveranstaltung
+	public long getCourseOfStudiesId() {
+		return courseOfStudiesId;
+	}
+
+	public void setCourseOfStudiesId(long courseOfStudiesId) {
+		this.courseOfStudiesId = courseOfStudiesId;
+	}
+
+	public long getCourseTemplateId() {
+		return courseTemplateId;
+	}
+
+	public void setCourseTemplateId(long courseTemplateId) {
+		this.courseTemplateId = courseTemplateId;
+	}
+
+	public long getSemesterId() {
+		return semesterId;
+	}
+
+	public void setSemesterId(long semesterId) {
+		this.semesterId = semesterId;
+	}
 
 }
