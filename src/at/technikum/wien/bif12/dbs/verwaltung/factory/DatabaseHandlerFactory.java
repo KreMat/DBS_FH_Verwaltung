978c22399ebd0e076d4336be041b9c4805ac5cd3
Matthias Kreuzriegler <matthias.kreuzriegler@gmx.at>
2014-01-12 16:42:12 +0100

    merge

--- src/at/technikum/wien/bif12/dbs/verwaltung/factory/DatabaseHandlerFactory.java
+++ src/at/technikum/wien/bif12/dbs/verwaltung/factory/DatabaseHandlerFactory.java
@@ -1,12 +1,15 @@
 package at.technikum.wien.bif12.dbs.verwaltung.factory;
 
 import at.technikum.wien.bif12.dbs.verwaltung.dao.DatabaseHandler;
-import at.technikum.wien.bif12.dbs.verwaltung.dao.mock.DatabaseHandlerMock;
+import at.technikum.wien.bif12.dbs.verwaltung.dao.impl.DatabaseHandlerImpl;
 
 public class DatabaseHandlerFactory {
 
 	public static DatabaseHandler createDatabaseHandler() {
-		return new DatabaseHandlerMock();
+		DatabaseHandlerImpl handler = new DatabaseHandlerImpl();
+		handler.init("jdbc:oracle:thin:@localhost:1521:xe", "fhverwaltung",
+				"123456");
+		return handler;
 	}
 
 }
--- src/at/technikum/wien/bif12/dbs/verwaltung/test/Main.java
+++ src/at/technikum/wien/bif12/dbs/verwaltung/test/Main.java
@@ -9,14 +9,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		DatabaseHandlerImpl dhm = new DatabaseHandlerImpl();
-		dhm.init();
+		dhm.init("jdbc:oracle:thin:@localhost:1521:XE", "thomas", "thomas");
+
+		Lektor l = new Lektor();
+		l.setFirstname("klaus");
+		l.setLastname("Kleber");
+		l.setAdress("Zirbenweg 5");
+		l.setZip("1200");
+		l.setTelefon("1231314321");
+		l.setEmail("klaus@gmx.com");
+		l.setToken("klkl");
+		l.setGehaltsklasse("A2");
 		
-		
-		//dhm.addLektor(new Lektor("penis","klaus","abs","1200","123412341","penx@asdfa.com","penx",1));
-		//dhm.addStudent(new Student("penis","klaus","abs","1200","123412341","penx@asdfa.com","Informatik",1210234089,"heinz"));
-		
-		
-		
+		dhm.addLektor(l);
+		System.out.println("Lektor added!");
+		// dhm.addLektor(new
+		// Lektor("penis","klaus","abs","1200","123412341","penx@asdfa.com","penx",1));
+		// dhm.addStudent(new
+		// Student("penis","klaus","abs","1200","123412341","penx@asdfa.com","Informatik",1210234089,"heinz"));
 
 	}
 
